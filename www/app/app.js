8406350dd57d20906735beeef389e2ce57b44983
Erwin Staal <Erwin Staal>
2015-11-04 11:54:16 +0100

    basic routing

--- www/app/app.js
+++ www/app/app.js
@@ -1,4 +1,11 @@
-angular.module('sandbox', [])
+angular.module('sandbox', ['ngRoute'])
+
+.config(function($routeProvider){
+	$routeProvider.when('/', {
+		template: '<h1>This is working!</h1>'
+	})
+})
+
 .controller('MainController', function($scope){
 	$scope.stuff = 'working!';
 });
