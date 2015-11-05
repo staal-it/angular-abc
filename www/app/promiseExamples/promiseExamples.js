e0044374a1d00c0de1b649a7ce800d3621718dc8
Erwin Staal <info@staal-it.nl>
2015-11-05 11:53:37 +0100

    promises pokemon api

--- www/app/promiseExamples/promiseExamples.js
+++ www/app/promiseExamples/promiseExamples.js
@@ -3,6 +3,44 @@
 	
 	angular.module('PromiseExamples', [])
 	
+	.factory('PokemonFactory', function($http, $q){
+		function getPokemon(id){
+			return $http.get('http://pokeapi.co/api/v1/pokemon/' + id + '/').then(function(response){
+				console.log('Pokemon: ', response);
+				return response.data;
+			});
+		}
+		
+		function getAbility(uri){
+			return $http.get('http://pokeapi.co/' + uri).then(function(response){
+				console.log('Ability: ', response.data);
+				return response.data;
+			});
+		}
+		
+		function getFullPokemon(id){
+			var pokemon;
+			
+			return getPokemon(id).then(function(data){
+				pokemon = data;
+				
+				var abilityPromises = [];
+				data.abilities.forEach(function(ability) {
+					abilityPromises.push(getAbility(ability.resource_uri))
+				}, this);
+				
+				return $q.all(abilityPromises).then(function(data){
+					pokemon.abilities = data;
+					return pokemon;
+				});
+			})
+		}
+		
+		return {
+				getFullPokemon: getFullPokemon
+			};
+	})
+	
 	.factory('SwapiFactory',  function($http){
 		function getPerson(id){
 			return $http.get('http://swapi.co/api/people/' + id + '/').then(function(response){
@@ -37,7 +75,7 @@
 		};
 	})
 	
-	.controller('PromiseController', function($http, SwapiFactory, $q, $timeout){
+	.controller('PromiseController', function($http, SwapiFactory, $q, $timeout, PokemonFactory){
 		var vm = this;
 		/*
 		var p = SwapiFactory.getPerson(1);
@@ -57,7 +95,7 @@
 			console.log(data);
 			vm.data = data;
 		});
-		*/
+		
 		
 		var tp = $timeout(function(){}, 3000).then(function(){
 			console.log('Timer');
@@ -85,5 +123,10 @@
 		$q.all(namedPromises).then(function(data){
 			console.log(data);
 		})
+		*/
+		
+		PokemonFactory.getFullPokemon(1).then(function(data){
+			vm.data = data;
+		});
 	});
 })();
