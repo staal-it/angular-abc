1e3c76ffb5554ed15f4f71ec22f061f3a2987309
Erwin Staal <info@staal-it.nl>
2015-11-05 10:47:46 +0100

    first promise with factory

--- www/app/promiseExamples/promiseExamples.js
+++ www/app/promiseExamples/promiseExamples.js
@@ -2,15 +2,32 @@
 	'use strict';
 	
 	angular.module('PromiseExamples', [])
-	.controller('PromiseController', function($http){
+	
+	.factory('SwapiFactory',  function($http){
+		function getPerson(id){
+			return $http.get('http://swapi.co/api/people/' + id + '/').then(function(response){
+				return response.data;
+			});
+		}
+		
+		return {
+			getPerson: getPerson
+		};
+	})
+	
+	.controller('PromiseController', function($http, SwapiFactory){
 		var vm = this;
 		
-		var p = $http.get('http://swapi.co/api/people/1/');
+		var p = SwapiFactory.getPerson(1);
 		
 		p.then(function(response){
-			return response.data;
-		}).then(function(response){
 			console.log(response);
 		});
+		
+		p.then(function(data){
+			return $http.get(data.homeworld);
+		}).then(function(response){
+			console.log(response.data);
+		})
 	});
 })();
